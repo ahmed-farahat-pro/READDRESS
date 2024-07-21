5d4812141d7cf95ce669b3ccd23dad53ceaca8c1
rowaidaahmady <118939263+ahmed-farahat-pro@users.noreply.github.com>
2024-07-22 01:03:08 +0300

    xvc

--- app/listings/show/page.js
+++ app/listings/show/page.js
@@ -3,6 +3,7 @@ import { useSearchParams } from 'next/navigation';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import styles from '../../styles/listingShow.module.css';
+import Slideshow from '../../components/SlideShow';
 
 export default function ListingShow() {
   const searchParams = useSearchParams();
@@ -17,13 +18,7 @@ export default function ListingShow() {
     <div className={styles.container}>
       <Header />
       <div className={styles.listingDetails}>
-        {listing.images.length > 0 && (
-          <img
-            src={listing.images[0].image_url}
-            alt="Listing Image"
-            className={styles.image}
-          />
-        )}
+       <Slideshow imageUrls= {listing.images}/>
         <h1>{listing.title}</h1>
         <p>{listing.description}</p>
         <p className={styles.price}>Price: {listing.price} EGB</p>
@@ -32,6 +27,12 @@ export default function ListingShow() {
         <p>Bedrooms: {listing.bedrooms}</p>
         <p>Bathrooms: {listing.bathrooms}</p>
         <p>Area: {listing.area} sq ft</p>
+         <a 
+      href={`tel:${listing.user_id.phone_number}`}
+      style={{ backgroundColor: 'green', color: 'white', padding: '10px', border: 'none', borderRadius: '5px', cursor: 'pointer', textDecoration: 'none', display: 'inline-block', marginTop: '10px' }}
+    >
+      Call
+    </a>
       </div>
       <Footer />
     </div>
