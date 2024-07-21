983fe6563fc44ac505874aeeebcf07048526f791
rowaidaahmady <118939263+ahmed-farahat-pro@users.noreply.github.com>
2024-07-21 16:15:33 +0300

    ahmed-farahat

--- app/listings/new/page.js
+++ app/listings/new/page.js
@@ -1,4 +1,5 @@
-"use client"
+'use client';
+
 import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import styles from '../../styles/Form.module.css';
@@ -8,16 +9,11 @@ import Header from '../../components/Header';
 
 export default function AddListing() {
   const searchParams = useSearchParams();
-   const [googleMapsUrl, setGoogleMapsUrl] = useState('');
-
-    const handleUrlChange = (url) => {
-        setGoogleMapsUrl(url);
-    };
-  const userId = searchParams.get('userId');
+  const [googleMapsUrl, setGoogleMapsUrl] = useState('');
   const [formData, setFormData] = useState({
-    user_id: userId,  // Replace with actual user ID or handle user authentication
+    user_id: searchParams.get('userId'),  // Replace with actual user ID or handle user authentication
     title: '',
-    maps_url:'',
+    maps_url: '',
     description: '',
     price: '',
     address: '',
@@ -33,14 +29,33 @@ export default function AddListing() {
     images: []
   });
   const [files, setFiles] = useState([]);
+  const [previews, setPreviews] = useState([]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleUrlChange = (url)=>{
+    setGoogleMapsUrl(url)
+  }
   const handleFileChange = (e) => {
-    setFiles([...e.target.files]);
+    const selectedFiles = Array.from(e.target.files);
+    setFiles(selectedFiles);
+
+    // Generate previews
+    const previewUrls = selectedFiles.map(file => URL.createObjectURL(file));
+    setPreviews(previewUrls);
+  };
+
+  const removeImage = (index) => {
+    // Remove from files
+    const updatedFiles = files.filter((_, i) => i !== index);
+    setFiles(updatedFiles);
+
+    // Remove from previews
+    const updatedPreviews = previews.filter((_, i) => i !== index);
+    setPreviews(updatedPreviews);
   };
 
   const uploadImage = async (file) => {
@@ -106,161 +121,171 @@ export default function AddListing() {
   };
 
   return (
+    <div>
+      <Header />
+      <div className={styles.container}>
+        <h1>Add New Listing</h1>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Description:
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              required
+              className={styles.textarea}
+            />
+          </label>
+          <label className={styles.label}>
+            Price:
+            <input
+              type="number"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Address:
+            <input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            City:
+            <input
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            State:
+            <input
+              type="text"
+              name="state"
+              value={formData.state}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Zip Code:
+            <input
+              type="text"
+              name="zip_code"
+              value={formData.zip_code}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Country:
+            <input
+              type="text"
+              name="country"
+              value={formData.country}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Property Type:
+            <input
+              type="text"
+              name="property_type"
+              value={formData.property_type}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Bedrooms:
+            <input
+              type="number"
+              name="bedrooms"
+              value={formData.bedrooms}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Bathrooms:
+            <input
+              type="number"
+              name="bathrooms"
+              value={formData.bathrooms}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Area:
+            <input
+              type="number"
+              name="area"
+              value={formData.area}
+              onChange={handleChange}
+              required
+              className={styles.input}
+            />
+          </label>
+          <label className={styles.label}>
+            Upload Images:
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              className={styles.input}
+            />
+          </label>
 
-    <div> 
-        <div> <Header/> </div>
-    <div className={styles.container}>
-      <h1>Add New Listing</h1>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <label className={styles.label}>
-          Title:
-          <input
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Description:
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-            className={styles.textarea}
-          />
-        </label>
-        <label className={styles.label}>
-          Price:
-          <input
-            type="number"
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Address:
-          <input
-            type="text"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          City:
-          <input
-            type="text"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          State:
-          <input
-            type="text"
-            name="state"
-            value={formData.state}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Zip Code:
-          <input
-            type="text"
-            name="zip_code"
-            value={formData.zip_code}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Country:
-          <input
-            type="text"
-            name="country"
-            value={formData.country}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Property Type:
-          <input
-            type="text"
-            name="property_type"
-            value={formData.property_type}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Bedrooms:
-          <input
-            type="number"
-            name="bedrooms"
-            value={formData.bedrooms}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Bathrooms:
-          <input
-            type="number"
-            name="bathrooms"
-            value={formData.bathrooms}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Area:
-          <input
-            type="number"
-            name="area"
-            value={formData.area}
-            onChange={handleChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <label className={styles.label}>
-          Upload Images:
-          <input
-            type="file"
-            multiple
-            onChange={handleFileChange}
-            required
-            className={styles.input}
-          />
-        </label>
-        <div>
+          <div className={styles.imagePreviewContainer}>
+            {previews.map((preview, index) => (
+              <div key={index} className={styles.imagePreview}>
+                <img src={preview} alt={`Preview ${index}`} className={styles.previewImage} />
+                <button type="button" onClick={() => removeImage(index)} className={styles.removeButton}>
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <div>
             <GoogleMaps onUrlChange={handleUrlChange} />
             <p>URL for clicked location: {googleMapsUrl}</p>
-        </div>
-        <button type="submit" className={styles.button}>Add Listing</button>
-      </form>
-    </div>
-<div> <Footer/> </div>
+          </div>
+          <button type="submit" className={styles.button}>Add Listing</button>
+        </form>
+      </div>
+      <Footer />
     </div>
   );
 }
