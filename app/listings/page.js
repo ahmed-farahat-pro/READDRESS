23339e4922b0bf102575861af1031e685c1dbaed
rowaidaahmady <118939263+ahmed-farahat-pro@users.noreply.github.com>
2024-07-21 16:00:53 +0300

    farahat

--- app/listings/page.js
+++ app/listings/page.js
@@ -1,11 +1,11 @@
 "use client";
-import { useState, useEffect,Suspense } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import styles from '../styles/listings.module.css';
 import Link from 'next/link';
-import GoogleMaps from '../components/GoogleMapsRender';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+
 export default function Listings() {
   const searchParams = useSearchParams();
   const userId = searchParams.get('userId');
@@ -13,9 +13,8 @@ export default function Listings() {
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
 
-
   useEffect(() => {
-    if (!userId) return; // Don't fetch listings until we have the userId
+    if (!userId) return;
 
     const fetchListings = async () => {
       try {
@@ -37,9 +36,7 @@ export default function Listings() {
   }, [userId]);
 
   const handleSearch = async () => {
-    if (!searchTerm) {
-      return; // Don't fetch listings if search term is empty
-    }
+    if (!searchTerm) return;
 
     try {
       const response = await fetch(`/api/listings/search/${searchTerm}`);
@@ -57,62 +54,51 @@ export default function Listings() {
   };
 
   if (!userId) {
-    return <div>Loading...</div>; // Show a loading state while waiting for userId
+    return <div>Loading...</div>;
   }
 
-  console.log('Listings page userId:', userId);
-
   return (
-     <Suspense fallback={<div>Loading...</div>}>
-    <div className={styles.container}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className={styles.container}>
         <Header />
-        
-      <Link href={`/listings/new?userId=${userId}`}>
-        <button>Add New Listing</button>
-      </Link>
-      <br></br>
+
+        <Link href={`/listings/new?userId=${userId}`}>
+          <button>Add New Listing</button>
+        </Link>
+        <br />
         <Link href={`/listings/edit?userId=${userId}`}>
-        <button>editListing</button>
-      </Link>
-      <h1>Listings</h1>
-      <div>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search by title"
-        />
-        <button onClick={handleSearch}>Search</button>
+          <button>Edit Listing</button>
+        </Link>
+        <h1>Listings</h1>
+        <div>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search by title"
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        {error && <p className={styles.error}>{error}</p>}
+        <div className={styles.listings}>
+          {listings.map((listing) => (
+            <Link key={listing._id} href={`/listings/${listing._id}`} className={styles.cardLink}>
+              <div className={styles.listing}>
+                {listing.images.length > 0 && (
+                  <img
+                    src={listing.images[0].image_url}
+                    alt="Listing Image"
+                    className={styles.image}
+                  />
+                )}
+                <h2>{listing.title}</h2>
+                <p className={styles.price}> {listing.price}  EGB</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <Footer />
       </div>
-      {error && <p className={styles.error}>{error}</p>}
-      <div className={styles.listings}>
-        {listings.map((listing) => (
-          <div key={listing._id} className={styles.listing}>
-            <h2>{listing.title}</h2>
-            <p>{listing.description}</p>
-            <p>Price: ${listing.price}</p>
-            <p>Bedrooms: {listing.bedrooms}</p>
-            <p>Bathrooms: {listing.bathrooms}</p>
-            <div className={styles.images}>
-              {listing.images.map((image, index) => (
-                <img key={index} src={image.image_url} alt={`Image ${index + 1}`} />
-              ))}
-            </div>
-                <div>
-      <h1>Google Maps Location</h1>
-      <GoogleMaps googleMapsUrl={listing.maps_url} />
-    </div>
-     <a 
-                href={`tel:${listing.user_id.phone_number}`}
-                style={{ backgroundColor: 'blue', color: 'white', padding: '10px', border: 'none', borderRadius: '5px', cursor: 'pointer', textDecoration: 'none', display: 'inline-block', marginTop: '10px' }}
-              >
-                Call
-              </a>
-          </div>
-        ))}
-      </div>
-      <Footer />
-    </div>
     </Suspense>
   );
 }
