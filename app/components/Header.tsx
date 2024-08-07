3b32d0a0ce56e829fcb5b004ece58b93df540ca2
rowaidaahmady <118939263+ahmed-farahat-pro@users.noreply.github.com>
2024-08-07 14:44:41 +0300

    farahat

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,20 +7,27 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
   return (
-    <header className="flex justify-between items-center p-4 bg-white text-gray-800 shadow-md">
-      <Link href="/" className="text-gray-800 hover:underline">
-        <div className="flex items-center text-xl font-bold">
-          <Image src="/re2.png" alt="Logo" height={30} width={100} className="mr-2" />
-        </div>
-      </Link>
-      
-      <nav className="flex gap-4">
+    <header className="flex items-center justify-between p-4 bg-white text-gray-800 shadow-md">
+      {/* Left-aligned space for logout or other items */}
+      <div className="flex-shrink-0">
         {isLoggedIn ? (
           <Link href="/" className="text-gray-800 hover:underline">
             Log Out
           </Link>
         ) : null}
-      </nav>
+      </div>
+
+      {/* Centered logo */}
+      <div className="flex-grow flex justify-center">
+        <Link href="/" className="text-gray-800 hover:underline">
+          <Image src="/Home.png" alt="Logo" height={50} width={125} /> {/* Adjusted dimensions */}
+        </Link>
+      </div>
+
+      {/* Right-aligned space for additional items if needed */}
+      <div className="flex-shrink-0">
+        {/* Add other items here if needed */}
+      </div>
     </header>
   );
 };
